831ec90caf6bb3d8bcb6314f66fa3c6ee916305a
Ikramullah <ikramullahzx@gmail.com>
2022-11-07 08:05:53 +0800

    fix: database tidak ikut hilang saat migrate down

---
+++ ProjectKonsentrasi.Migrator/List/20221108_AdminLogin.cs
@@ -0,0 +1,21 @@
+using FluentMigrator;
+
+namespace ProjectKonsentrasi.Migrator.List;
+[Migration(20221108)]
+public class _20221108_AdminLogin : Migration
+{
+    public override void Up()
+    {
+        this.Create.Table("AdminUser")
+            .WithColumn("ID").AsInt64().NotNullable().PrimaryKey().Identity()
+            .WithColumn("Nama").AsString().NotNullable()
+            .WithColumn("Email").AsString().NotNullable()
+            .WithColumn("Password").AsString().NotNullable()
+            .WithColumn("CreatedAt").AsString().NotNullable();
+    }
+
+    public override void Down()
+    {
+        this.Delete.Table("AdminUser");
+    }
+}
