08d7f0a4fa6da0e8de94b0a40bab3406ef5795a5
Ikramullah <ikramullahzx@gmail.com>
2022-12-21 23:19:20 +0800

    feat: persiapan faker dan cek validasi pengisian data (dasar)

--- ProjectKonsentrasi.Webserver/Controllers/RegisterPatientController.cs
+++ ProjectKonsentrasi.Webserver/Controllers/RegisterPatientController.cs
@@ -5,10 +5,18 @@ namespace ProjectKonsentrasi.Webserver.Controllers;
 public class RegisterPatientController : Controller
 {
     private DBContext _db = new DBContext();
-    
+
     [HttpGet("register_patient")]
-    public IActionResult Index()
+    public IActionResult Index([FromQuery] bool? withFaker)
     {
+        // Buat faker
+        ViewBag.WithFaker = withFaker ?? false;
         return View();
+    }
+
+    [HttpPost("register_patient/take_picture")]
+    public IActionResult TakePicture([FromForm] IFormCollection form)
+    {
+        return Json("Reserved");
     }
 }
